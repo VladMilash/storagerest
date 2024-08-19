7204d31af275d13685558a69caf68101bc76b777
Vlad_Milas <12milash12@gmail.com>
2024-08-19 14:14:24 +0300

    created tables for db

---
+++ src/main/resources/db/migration/V1__19082024.sql
@@ -0,0 +1,24 @@
+CREATE TABLE user
+(
+    id   INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(2048) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    role VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE file
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    location VARCHAR(50) NOT NULL,
+    status VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE event
+(
+    status VARCHAR(50) NOT NULL,
+    user_id INT,
+    file_id INT,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (file_id) REFERENCES file(id)
+);
