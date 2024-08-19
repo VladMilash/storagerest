3c2a4687b564672aa090ad900d505aa77fbf2f6c
Vlad_Milas <12milash12@gmail.com>
2024-08-19 12:13:33 +0300

    configured project

---
+++ src/main/java/com/mvo/storagerest/StorageRestApplication.java
@@ -0,0 +1,13 @@
+package com.mvo.storagerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageRestApplication.class, args);
+	}
+
+}
